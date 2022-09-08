ced75390cda0b8cf354725ae1e326087516c5a73
Ananya Kulkarni <ananyakulkarni1203@gmail.com>
2022-09-08 19:44:57 +0530

    Creation and traversal of a binary tree

--- Binary_Tree/Creation_traversal.cpp
+++ Binary_Tree/Creation_traversal.cpp
@@ -1,87 +1,132 @@
 #include <iostream>
+#include <queue>
 using namespace std;
-class TreeNode
+struct TreeNode
 {
-private:
     int data;
     TreeNode *left;
     TreeNode *right;
-
-public:
-    friend class Tree;
-    TreeNode()
-    {
-        data = 0;
-        left = NULL;
-        right = NULL;
-    }
 };
-class Tree
+void Level_order(TreeNode *root)
 {
-private:
-    TreeNode *root;
-
-public:
-    Tree();
-    void insertion(TreeNode *Root, TreeNode *newnode)
+    if (root == NULL)
     {
-        root=Root;
-        newnode = new TreeNode();
-        cout << "Enter the number that you want to insert" << endl;
-        int x;
-        cin >> x;
-        newnode->data = x;
-        char option;
-        cout << "Enter where do you want to insert TreeNode. Left or Right" << endl;
-        cin >> option;
-        if (option == 'r')
+        // cout << "Tree is empty" << endl;
+        return;
+    }
+    queue<TreeNode *> Q;
+    Q.push(root);
+    while (!Q.empty())
+    {
+        TreeNode *temp = Q.front();
+        cout << temp->data;
+        if (temp->left != NULL)
         {
-            if (root->right == NULL)
-            {
-                root->right = newnode;
-            }
-            else
-            {
-                insertion(root->right, newnode);
-            }
+            Q.push(temp->left);
+        }
+        if (temp->right != NULL)
+        {
+            Q.push(temp->right);
+        }
+        Q.pop();
+    }
+}
+void Inorder(TreeNode *root)
+{
+    if (root == NULL)
+    {
+        // cout << "Tree is empty" << endl;
+        return;
+    }
+    Inorder(root->left);
+    cout << root->data << " ";
+    Inorder(root->right);
+}
+void Preorder(TreeNode *root)
+{
+    if (root == NULL)
+    {
+        // cout << "Tree is empty" << endl;
+        return;
+    }
+    cout << root->data << " ";
+    Preorder(root->left);
+    Preorder(root->right);
+}
+void Postorder(TreeNode *root)
+{
+    if (root == NULL)
+    {
+        // cout << "Tree is empty" << endl;
+        return;
+    }
+    Postorder(root->left);
+    Postorder(root->right);
+    cout << root->data << " ";
+}
+void insertion(TreeNode *root, TreeNode *newnode)
+{
+    char option;
+    cout << "Enter where do you want to insert Left or Right of " << root->data << endl;
+    cin >> option;
+    if (option == 'r')
+    {
+        if (root->right == NULL)
+        {
+            root->right = newnode;
         }
         else
         {
-            if (root->left == NULL)
-            {
-                root->left = newnode;
-            }
-            else
-            {
-                insertion(root->left, newnode);
-            }
+            insertion(root->right, newnode);
         }
     }
-};
-
-Tree::Tree()
-{
-    root = NULL;
+    else
+    {
+        if (root->left == NULL)
+        {
+            root->left = newnode;
+        }
+        else
+        {
+            insertion(root->left, newnode);
+        }
+    }
 }
-
+TreeNode *get_node()
+{
+    TreeNode *temp;
+    temp = new TreeNode;
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
 int main()
 {
-    Tree T1;
-    TreeNode *Root, *newnode;
-
-    cout << "Do you want to insert elements? " << endl;
+    TreeNode *newnode, *root;
+    root = NULL;
     char option;
-    cin >> option;
-    if (option == 'y')
-    {
-        cout << "How many elements do you want to insert ?" << endl;
-        int n;
-        cin >> n;
-        for (int i = 0; i < n; i++)
-        {
-            T1.insertion(Root, newnode);
-        }
-    }
 
+    do
+    {
+        newnode = get_node();
+        cout << "Enter the number that you want to insert " << endl;
+        int x;
+        cin >> x;
+        newnode->data = x;
+        if (root == NULL)
+
+            root = newnode;
+        else
+            insertion(root, newnode);
+
+        cout << "Do you want to enter element ? ";
+        cin >> option;
+    } while (option == 'y');
+    cout << "Inorder Traversal" << endl;
+    Inorder(root);
+    cout << "\nPreorder Traversal" << endl;
+    Preorder(root);
+    cout << "\nPostorder Traversal" << endl;
+    Postorder(root);
     return 0;
 }
